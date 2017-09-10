e470264d2b12d7b2788a9256f4de659d31114543
Pac12Run8000 <norbertgrover@michelles-macbook-pro.local>
2017-09-09 20:25:28 -0700

    Added other videocontrollers

---
+++ OnTheMap2/AddLocationViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AddLocationViewController.swift
+//  OnTheMap2
+//
+//  Created by Michelle Grover on 9/9/17.
+//  Copyright © 2017 Norbert Grover. All rights reserved.
+//
+
+import UIKit
+
+class AddLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+   
+
+}
